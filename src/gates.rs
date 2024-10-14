8ba06c90a95ecbdc8657881d6ceef00b79a8b839
Diseyi <filomiinaa@gmail.com>
2024-10-14 18:55:38 +0100

    add tests

--- src/gates.rs
+++ src/gates.rs
@@ -85,3 +85,62 @@ impl Logic for XNOR {
 //         self.logic()
 //     }
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_and() {
+        assert!(AND { a: true, b: true }.logic());
+        assert!(!AND { a: true, b: false }.logic());
+        assert!(!AND { a: false, b: true }.logic());
+        assert!(!AND { a: false, b: false }.logic());
+    }
+
+    #[test]
+    fn test_or() {
+        assert!(OR { a: true, b: true }.logic());
+        assert!(OR { a: true, b: false  }.logic());
+        assert!(OR { a: false, b: true}.logic());
+        assert!(!OR { a: false, b: false }.logic());
+    }
+
+    #[test]
+    fn test_not() {
+        assert!(!NOT { a: true }.logic());
+        assert!(NOT { a: false }.logic());
+    }
+
+    #[test]
+    fn test_nand() {
+        assert!(!NAND { a: true, b: true }.logic());
+        assert!(NAND { a: true, b: false  }.logic());
+        assert!(NAND { a: false, b: true}.logic());
+        assert!(NAND { a: false, b: false }.logic());
+    }
+
+    #[test]
+    fn test_nor() {
+        assert!(!NOR { a: true, b: true }.logic());
+        assert!(!NOR { a: true, b: false  }.logic());
+        assert!(!NOR { a: false, b: true }.logic());
+        assert!(NOR {a: false, b: false}.logic());
+    }
+
+    #[test]
+    fn test_xor() {
+        assert!(!XOR { a: true, b: true }.logic());
+        assert!(XOR { a: true, b: false  }.logic());
+        assert!(XOR { a: false, b: true }.logic());
+        assert!(!XOR { a: false, b: false }.logic());
+    }
+
+    #[test]
+    fn test_xnor() {
+        assert!(XNOR { a: true, b: true }.logic());
+        assert!(!XNOR { a: true, b: false  }.logic());
+        assert!(!XNOR { a: false, b: true}.logic());
+        assert!(XNOR { a: false, b: false }.logic());
+    }
+}
